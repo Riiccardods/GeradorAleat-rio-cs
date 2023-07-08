2794668e97e62b0fc5f931c792b2f06a3adf0981
Riiccardods <riicardoljn@icloud.com>
2023-07-07 22:01:49 -0300

    Adicionar arquivos de projeto.

---
+++ trader/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿using System.Drawing;
+
+namespace trader
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.lblNumber.Location = new System.Drawing.Point(100, 50);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(100, 100);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNumber.Click += new System.EventHandler(this.lblNumber_Click);
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(100, 200);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(100, 50);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capturar";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(53, 274);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(337, 524);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(423, 274);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(348, 524);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Capturar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Capturar duplo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(523, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(709, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 72);
+            this.label1.Location = new System.Drawing.Point(709, 106);
+            this.Controls.Add(label1);
+
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(884, 861);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.lblNumber);
+            this.MinimumSize = new System.Drawing.Size(900, 900);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ trader/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace trader
+{
+    public partial class Form1 : Form
+    {
+        int contador1 = 0;
+        int contador2 = 0;
+
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblNumber.Text = "0";  // Inicia o lblNumber com o valor "0"
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Verifica o número atual e muda para o outro número
+            lblNumber.Text = lblNumber.Text == "0" ? "1" : "0";
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+           
+            listBox1.Text = listBox1.Items.Count.ToString();
+            string number = lblNumber.Text;
+            listBox1.Items.Insert(0,$"-{number}");
+        }
+
+       
+
+    private void lblNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Text = listBox2.Items.Count.ToString();
+            string number = lblNumber.Text;
+            listBox2.Items.Insert(0, $"-{number}");
+        }
+
+        private void ButtonClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            contador1++;
+            label1.Text = "Contagem: " + contador1;
+            listBox2.Items.Insert(0,lblNumber.Text);
+            listBox1.Items.Insert(0,lblNumber.Text);
+            string number = "-" + lblNumber.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            contador1 = 0;
+            label1.Text = "Contagem: 0";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+     
+
+        }
+    }
+}
