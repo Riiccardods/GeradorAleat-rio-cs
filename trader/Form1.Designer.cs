bba26fe9c87f395a70791cf69358382c07ee4054
Riiccardods <riicardoljn@icloud.com>
2023-07-13 23:38:52 -0300

    2

--- trader/Form1.Designer.cs
+++ trader/Form1.Designer.cs
@@ -26,20 +26,18 @@ namespace trader
             this.btnCapture = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.listBox3 = new System.Windows.Forms.ListBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
             this.labelWinCount = new System.Windows.Forms.Label();
             this.labelLossCount = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNumber
@@ -77,28 +75,7 @@ namespace trader
             // timer1
             // 
             this.timer1.Interval = 1;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // listBox2
-            // 
-            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(495, 374);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(348, 424);
-            this.listBox2.TabIndex = 3;
-            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(495, 295);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 50);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "BAIXO";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.timer1.Tick += new System.EventHandler(this.Timer1_TickEvent);
             // 
             // button2
             // 
@@ -129,76 +106,6 @@ namespace trader
             this.label1.TabIndex = 0;
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
-            // listBox3
-            // 
-            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 20;
-            this.listBox3.Location = new System.Drawing.Point(959, 374);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(348, 424);
-            this.listBox3.TabIndex = 7;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(53, 220);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 50);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "CIMA";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(495, 220);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(100, 50);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "CIMA";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(1207, 295);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(100, 50);
-            this.button6.TabIndex = 10;
-            this.button6.Text = "BAIXO";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(1207, 220);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(100, 50);
-            this.button7.TabIndex = 11;
-            this.button7.Text = "CIMA";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(959, 295);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(100, 50);
-            this.button8.TabIndex = 12;
-            this.button8.Text = "BAIXO - BAIXO";
-            this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(959, 220);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(100, 50);
-            this.button9.TabIndex = 13;
-            this.button9.Text = "BAIXO - CIMA";
-            this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
-            // 
             // labelWinCount
             // 
             this.labelWinCount.BackColor = System.Drawing.SystemColors.ActiveCaption;
@@ -214,23 +121,94 @@ namespace trader
             this.labelLossCount.Name = "labelLossCount";
             this.labelLossCount.Size = new System.Drawing.Size(125, 31);
             this.labelLossCount.TabIndex = 15;
+            this.labelLossCount.Click += new System.EventHandler(this.labelLossCount_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(495, 374);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(348, 424);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "BAIXO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(430, 163);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(193, 51);
+            this.lblSaldo.TabIndex = 17;
+            this.lblSaldo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSaldo.Click += new System.EventHandler(this.lblSaldo_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(225, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 50);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "DESBLOQUEAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(916, 374);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(348, 424);
+            this.listBox3.TabIndex = 19;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1164, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 50);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "BAIXO";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(916, 295);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 50);
+            this.button7.TabIndex = 22;
+            this.button7.Text = "BAIXO BAIXO";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1384, 861);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelLossCount);
             this.Controls.Add(this.labelWinCount);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.listBox3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnCapture);
@@ -238,24 +216,20 @@ namespace trader
             this.MinimumSize = new System.Drawing.Size(1400, 900);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
-
-        private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Label labelWinCount;
         private System.Windows.Forms.Label labelLossCount;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
     }
 }
--- trader/Form1.cs
+++ trader/Form1.cs
@@ -1,54 +1,91 @@
 ﻿using System;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace trader
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+
         int contador1 = 0;
         int contadorWin = 0;
         int contadorLoss = 0;
-
-        private Random random = new Random();
+        int contadorExtra = 0;
+        int sequenciaLoss = 0;
+        int sequenciawin = 0;
+        double saldo = 0.0;
 
         public Form1()
         {
             InitializeComponent();
+            timer.Interval = 1;  // Intervalo de 1 segundo
+            timer.Tick += Timer1_TickEvent;  // Evento que será acionado a cada intervalo de tempo
+            timer.Start();  // Inicia o temporizador
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private int numberCounter = 0;
+
+        private void Timer1_TickEvent(object sender, EventArgs e)
         {
-            lblNumber.Text = "0";  // Inicia o lblNumber com o valor "0"
-            timer1.Start();
+            numberCounter = (numberCounter == 0) ? 1 : 0;  // Alterna entre 0 e 1
+
+            lblNumber.Text = numberCounter.ToString();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            // Verifica o número atual e muda para o outro número
-            lblNumber.Text = lblNumber.Text == "0" ? "1" : "0";
-        }
+
+        private bool saldoCongelado = false;
 
         private void btnCapture_Click(object sender, EventArgs e)
         {
-            contador1++;
-            label1.Text = "Contagem: " + contador1;
 
             string number = lblNumber.Text;
+
+            if (saldoCongelado) // Verificar se o saldo está congelado
+            {
+
+                return; // Interromper a atualização do saldo
+            }
 
             if (number == "0")
             {
                 listBox1.Items.Insert(0, "LOSS");
                 contadorLoss++;
+                
+                sequenciaLoss++;
+                sequenciawin = 0; // Redefinir a sequência de "LOSS"
+
+
+                if (sequenciaLoss == 9) // Verificar se a sequência de "LOSS" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+                
             }
-            else if (number == "1")
+            else
             {
                 listBox1.Items.Insert(0, "WIN");
                 contadorWin++;
+               
+                sequenciawin++;
+                sequenciaLoss = 0; // Redefinir a sequência de "LOSS"
+
+                if (sequenciawin == 9) // Verificar se a sequência de "win" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
             }
+
+            contador1++;
+            label1.Text = "Contagem: " + contador1;
 
             labelWinCount.Text = "Wins: " + contadorWin;
             labelLossCount.Text = "Losses: " + contadorLoss;
+            lblSaldo.Text = "Saldo: " + saldo.ToString("0.00"); // Atualizar o rótulo do saldo
         }
 
        
@@ -93,38 +130,66 @@ namespace trader
 
         private void button2_Click(object sender, EventArgs e)
         {
-            contador1++;
-            label1.Text = "Contagem: " + contador1;
-
             string number = lblNumber.Text;
+
+            if (saldoCongelado) // Verificar se o saldo está congelado
+            {
+
+                return; // Interromper a atualização do saldo
+            }
 
             if (number == "0")
             {
                 listBox1.Items.Insert(0, "LOSS");
                 listBox2.Items.Insert(0, "LOSS");
                 contadorLoss++;
+                
+                sequenciaLoss++;
+                sequenciawin++;
+
+                if (sequenciaLoss == 11) // Verificar se a sequência de "LOSS" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+                if (sequenciawin == 11) // Verificar se a sequência de "win" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
             }
-            else if (number == "1")
+            else
             {
                 listBox1.Items.Insert(0, "WIN");
                 listBox2.Items.Insert(0, "WIN");
                 contadorWin++;
+               
+                sequenciaLoss = 0; // Redefinir a sequência de "LOSS"
+                sequenciawin = 0; // Redefinir a sequência de "LOSS"
             }
+
+            contador1++;
+            label1.Text = "Contagem: " + contador1;
 
             labelWinCount.Text = "Wins: " + contadorWin;
             labelLossCount.Text = "Losses: " + contadorLoss;
+            lblSaldo.Text = "Saldo: " + saldo.ToString("0.00"); // Atualizar o rótulo do saldo
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            saldoCongelado = false; // Congelar o saldo
             listBox1.Items.Clear();
             listBox2.Items.Clear();
+            listBox3.Items.Clear();
             contador1 = 0;
             contadorWin = 0;
             contadorLoss = 0;
             label1.Text = "Contagem: 0";
             labelWinCount.Text = "Contagem: 0";
             labelLossCount.Text = "Contagem: 0";
+            lblSaldo.Text = "0";
+            saldo = 0;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -176,54 +241,29 @@ namespace trader
             }
         }
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-            string number = lblNumber.Text;
-
-            if (number == "0")
-            {
-                listBox3.Items.Insert(0, "LOSS");
-                contadorLoss++;
-            }
-            else if (number == "1")
-            {
-                listBox3.Items.Insert(0, "WIN");
-                contadorWin++;
-            }
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            string number = lblNumber.Text;
-
-            if (number == "1")
-            {
-                listBox3.Items.Insert(0, "LOSS");
-                contadorLoss++;
-            }
-            else if (number == "0")
-            {
-                listBox3.Items.Insert(0, "WIN");
-                contadorWin++;
-            }
-        }
 
         private void button8_Click(object sender, EventArgs e)
         {
+
             string number = lblNumber.Text;
 
             if (number == "0")
             {
                 listBox2.Items.Insert(0, "LOSS");
-                listBox3.Items.Insert(0, "LOSS");
-                contadorLoss++;
+                contadorWin++;
             }
             else if (number == "1")
             {
                 listBox2.Items.Insert(0, "WIN");
-                listBox3.Items.Insert(0, "WIN");
-                contadorWin++;
+                contadorLoss++;
+                contadorExtra++;
             }
+
+            labelWinCount.Text = "Wins: " + contadorWin;
+            labelLossCount.Text = "Losses: " + contadorLoss;
+
+
+
         }
 
         private void button9_Click(object sender, EventArgs e)
@@ -233,15 +273,217 @@ namespace trader
             if (number == "0")
             {
                 listBox2.Items.Insert(0, "LOSS");
-                listBox3.Items.Insert(0, "WIN");
                 contadorLoss++;
+                contadorExtra++;
             }
+            
             else if (number == "1")
             {
                 listBox2.Items.Insert(0, "WIN");
-                listBox3.Items.Insert(0, "LOSS");
                 contadorWin++;
             }
+
+        }
+
+        private void labelLossCount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            string number = lblNumber.Text;
+
+            if (saldoCongelado) // Verificar se o saldo está congelado
+            {
+
+                return; // Interromper a atualização do saldo
+            }
+
+            if (number == "0")
+            {
+                listBox2.Items.Insert(0, "LOSS");
+                contadorLoss++;
+                saldo -= 1.00; // Subtrair 1.00 do saldo
+                sequenciaLoss++;
+                sequenciawin = 0; // Redefinir a sequência de "LOSS"
+
+
+                if (sequenciaLoss == 5) // Verificar se a sequência de "LOSS" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+
+            }
+            else
+            {
+                listBox2.Items.Insert(0, "WIN");
+                contadorWin++;
+                saldo += 0.95; // Adicionar 0.95 ao saldo
+                sequenciawin++;
+                sequenciaLoss = 0; // Redefinir a sequência de "LOSS"
+
+                if (sequenciawin == 5) // Verificar se a sequência de "win" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+            }
+        }
+
+        private void lblSaldo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            saldoCongelado = false; // Congelar o saldo
+            contador1 = 0;
+            label1.Text = "Contagem: 0";
+        }
+
+        private bool button2Clicking = false;
+        private int button2ClickCounter = 0;
+
+        private void button2AutoClick_Click(object sender, EventArgs e)
+        {
+            int totalClicks = 100;
+            int delayMilliseconds = 50;
+
+            if (!button2Clicking)
+            {
+                button2Clicking = true;
+
+                for (int i = 0; i < totalClicks; i++)
+                {
+                    button2_Click(sender, e);
+                    button2ClickCounter++;
+
+                    if (button2ClickCounter >= totalClicks)
+                    {
+                        button2ClickCounter = 0;
+                        button2Clicking = false;
+                        break;
+                    }
+
+                    Thread.Sleep(delayMilliseconds);
+                }
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+
+            string number = lblNumber.Text;
+
+            if (saldoCongelado) // Verificar se o saldo está congelado
+            {
+
+                return; // Interromper a atualização do saldo
+            }
+
+            if (number == "0")
+            {
+                listBox3.Items.Insert(0, "LOSS");
+                contadorLoss++;
+                saldo -= 1.00; // Subtrair 1.00 do saldo
+                sequenciaLoss++;
+                sequenciawin = 0; // Redefinir a sequência de "LOSS"
+
+
+                if (sequenciaLoss == 9) // Verificar se a sequência de "LOSS" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+
+            }
+            else
+            {
+                listBox3.Items.Insert(0, "WIN");
+                contadorWin++;
+                saldo += 0.95; // Adicionar 0.95 ao saldo
+                sequenciawin++;
+                sequenciaLoss = 0; // Redefinir a sequência de "LOSS"
+
+                if (sequenciawin == 9) // Verificar se a sequência de "win" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+            }
+
+            contador1++;
+            label1.Text = "Contagem: " + contador1;
+
+            labelWinCount.Text = "Wins: " + contadorWin;
+            labelLossCount.Text = "Losses: " + contadorLoss;
+            lblSaldo.Text = "Saldo: " + saldo.ToString("0.00"); // Atualizar o rótulo do saldo
+        }
+    
+
+
+
+    private void button7_Click(object sender, EventArgs e)
+        {
+            string number = lblNumber.Text;
+
+            if (saldoCongelado) // Verificar se o saldo está congelado
+            {
+
+                return; // Interromper a atualização do saldo
+            }
+
+            if (number == "0")
+            {
+                listBox1.Items.Insert(0, "LOSS");
+                listBox2.Items.Insert(0, "LOSS");
+                listBox3.Items.Insert(0, "LOSS");
+                contadorLoss++;
+                saldo -= 0.95; // Adicionar 0.95 ao saldo
+                sequenciaLoss++;
+                sequenciawin = 0; // Redefinir a sequência de "LOSS"
+
+                if (sequenciaLoss == 10) // Verificar se a sequência de "LOSS" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+
+            }
+            else
+            {
+                listBox1.Items.Insert(0, "WIN");
+                listBox2.Items.Insert(0, "WIN");
+                listBox3.Items.Insert(0, "WIN");
+                contadorWin++;
+                saldo += 1.00; // Adicionar 0.95 ao saldo
+                sequenciawin++;
+                sequenciaLoss = 0; // Redefinir a sequência de "LOSS"
+
+                if (sequenciawin == 10) // Verificar se a sequência de "win" atingiu 10
+                {
+
+                    saldoCongelado = true; // Congelar o saldo
+                }
+            }
+
+            contador1++;
+            label1.Text = "Contagem: " + contador1;
+
+            labelWinCount.Text = "Wins: " + contadorWin;
+            labelLossCount.Text = "Losses: " + contadorLoss;
+            lblSaldo.Text = "Saldo: " + saldo.ToString("0.00"); // Atualizar o rótulo do saldo
         }
     }
 }
+
+
